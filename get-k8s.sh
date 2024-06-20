6b9cbc6bb21fd3fcf18a9edbfc7b67392856b968
Niru0290 <90973750+Niru0290@users.noreply.github.com>
2024-06-20 18:41:16 +0530

    Update get-k8s.sh

--- get-k8s.sh
+++ get-k8s.sh
@@ -5,3 +5,5 @@ echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.
 sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl
+sudo rm /etc/containerd/config.toml
+sudo systemctl restart containerd
